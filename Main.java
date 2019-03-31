760f4b9dc909625ec57df1d52754ca0e5b71d98b
govindshukla213 <govindshukla213@gmail.com>
2019-03-31 14:53:14 +0530

    solved probl import java.io.BufferedReader;

--- Main.java
+++ Main.java
@@ -7,28 +7,42 @@ import java.io.InputStreamReader;
      
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
           
-    int n= Integer.parseInt(br.readLine());
-         String s[]= new String[n];
-        for( int i=0;i<n;i++)
-        {
-            s[i]= br.readLine(); 
-        }
-        for( int i=0;i<n;i++)
-        {
-           if(s[i].length()>10)
-           {
-               System.out.printf("%c%d%c\n",s[i].charAt(0),s[i].length()-2,s[i].charAt(s[i].length()-1));
+          int n= Integer.parseInt(br.readLine());
+          int a[][] = new int[n][3]; 
+          String str[];
+         
+          for(int i=0;i<n;i++)
+          {
+              str=br.readLine().split("\\s");
+                for(int j=0;j<3;j++)
+            {
+                    a[i][j]= Integer.parseInt(str[j]);
+            }
+          }
+          int c=0,sol;
+             sol=0;
+                
 
+           for(int i=0;i<n;i++)
+          {
+              c=0;
+                for(int j=0;j<3;j++)
+              {
+                  if(a[i][j]==1)
+                    c++;
+
+                   if(c==2)
+                   {
+                        sol++;
+                       break;
+                   }    
+              }
            }
-           else{
-               System.out.println(s[i]);
-           }
-           
-        }
+           System.out.println(sol);
 
     }   
-  }
   
+}
   
   
 
