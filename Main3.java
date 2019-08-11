8a05c6de196cab2024042aab4c05f8809a1ccaaf
govindshukla213 <govindshukla213@gmail.com>
2019-08-12 01:44:13 +0530

    Solved codeforces #455A Boredum

--- Main3.java
+++ Main3.java
@@ -1,51 +1,33 @@
-import java.util.Scanner;
-
+import java.io.*;
+import java.lang.Math;
 public class Main3
 {
-    static int maxcount(int n ,int a[])
-    {  
-          int c=0,dx,max=0;
-        
-        for(int i=0;i<n-1;i++)
-        { 
-            dx=a[i+1]-a[i];
-              if(dx>=0)
-              {
-                  c++;
-              }
-              else
-              { 
-                 if(c>max)
-                 {
-                     max=c;
-                 }
-                  c=0;
-              }
-        
-            
-        }
-        if(c>max)
-        {
-            max=c;
-        }
-        return(max+1);
+    
+    public static void main(String[]args) throws IOException 
+    {
 
-    }
-       
-    public static void main(String[]args) {
-
-        Scanner sc = new Scanner(System.in);
-           int n=sc.nextInt();
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        int max=100001;
+           int n=Integer.parseInt(br.readLine());
            int a[]=new int[n];
+            long freq[]=new long[max];
+            long  dp[]=new  long[max];
+           String s[]=br.readLine().split("\\s");
             for(int i=0;i<n;i++)
             { 
-               a[i]=sc.nextInt();
+               a[i]=Integer.parseInt(s[i]);
+               freq[a[i]]+=a[i];
+            }
+            dp[0]=0;
+            dp[1]=freq[1];
+            long res=0;
+            for(int i=2;i<max;i++)
+            {
+                dp[i]=Math.max(dp[i-1],dp[i-2]+freq[i]);
+                res = Math.max(res,dp[i]);
             }
 
-            System.out.println(maxcount(n,a));
-        
-
-
-        
+            System.out.println(res);
+             
     }
 }
